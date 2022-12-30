3f8a9c3e7d8227c0ae7d056158c39c9695875bf7
Spoon <nycbasic@gmail.com>
2022-12-30 11:14:00 -0500

    working on forgot password patch route

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -28,23 +28,40 @@ const checkUser = async (req, res) => {
       const email = resetPasswordEmail(user.email, token);
       const info = await transporter.sendMail(email);
       // send a response to user "email sent!"
-      console.log(email);
-      return res.status(200).json({
-        status: "Local user",
-        token,
-      });
+      if (info.accepted) {
+        return res.status(200).json({
+          status: "Local user",
+          token,
+        });
+      }
     }
     // return response that the user does not exist
     return res.status(400).json({
-      status: "User cannot reset password!",
+      status:
+        "User cannot reset password! Either user already received email or useer does not exist",
     });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return res.sendStatus(400);
   }
 };
 
 const forgotPassword = (req, res) => {
+  // Check if reset token exist and verify user
+  const {
+    params: { token },
+    body: { newPassword, confirmPassword },
+  } = req;
+  try {
+    // If token matches user and user information is correct
+    const user = User.findOne({ where: { resetToken: req.token } });
+    // accept password change
+    // Send confirmation
+    // If there's an issue, send error
+  } catch (err) {
+    return res.sendStatus(400);
+  }
+
   return res.status(200).json({
     message: "forgot password endpoint success",
     data: req.body,
